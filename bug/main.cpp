7a60e95507214b0125c92b892b91ac523b7fde85
samsarabbi007 <bsse0916@iit.du.ac.bd>
2018-01-25 16:34:04 +0600

    first commit

---
+++ bug/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=5;
+    for(int y=10;y>=0;y--)
+    {
+        int z=50/(y-x);
+        cout<<z<<endl;
+    }
+
+    return 0;
+}
---
+++ debug/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x=5;
+    for(int y=10;y>=0;y--)
+    {
+        int z=50/(y-x);
+        cout<<z<<endl;
+    }
+
+    return 0;
+}
